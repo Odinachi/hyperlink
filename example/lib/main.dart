a8740f3308f2174a4ad03073c8d296137c9c0ae8
Odinachi David <odinachidavid@yahoo.com>
2024-05-13 13:41:52 +0100

    improve example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,14 +8,15 @@ void main() {
         title: const Text('Link Rich Text Example'),
       ),
       body: const Center(
-        child: HyperLink(
-          textStyle: TextStyle(color: Colors.black),
-          linkStyle: TextStyle(
-            color: Colors.red,
-            fontWeight: FontWeight.w700,
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: HyperLink(
+            textStyle: TextStyle(color: Colors.black, fontSize: 15),
+            linkStyle: TextStyle(
+                color: Colors.red, fontWeight: FontWeight.w700, fontSize: 20),
+            text:
+                'Click here to visit (Google)[https://www.google.com] or Click here to visit (Apple)[https://www.apple.com]\t Happy Coding!!',
           ),
-          text:
-              'Click here to visit (Google)[https://www.google.com] or Click here to visit (Apple)[https://www.apple.com]',
         ),
       ),
     ),
